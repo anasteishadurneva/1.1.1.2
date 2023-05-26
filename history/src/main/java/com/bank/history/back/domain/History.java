b3f52593dd3eab6e470e80f5c426b217335bac10
Nastya <anasteishadurneva@yandex.ru>
2023-05-26 17:05:59 +0300

    first commit

---
+++ history/src/main/java/com/bank/history/back/domain/History.java
@@ -0,0 +1,34 @@
+package com.bank.history.back.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "history")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String action;
+
+    private String details;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+}
---
+++ history/src/main/java/com/bank/history/back/mapping/HistorySummaryProjectionMapper.java
@@ -0,0 +1,7 @@
+package com.bank.history.back.mapping;
+
+import com.bank.history.back.domain.History;
+import com.bank.history.back.dto.HistorySummaryProjectionDto;
+
+public class HistorySummaryProjectionMapper {
+}
